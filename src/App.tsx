1af29ad2cb85e1ebfe45166b5d3214efa70c7e8c
mikel-js <michaelcastro.js@gmail.com>
2022-08-23 23:02:36 +0300

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,29 @@
-import React from 'react';
+import React, { lazy, FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home/index'));
+
+type Props = {
+  component: FC;
+  path: any;
+};
+
+const CustomRoute: FC<Props> = ({ component: Component, path }) => {
+  return (
+    <Route
+      path={path}
+      element={(props: any): any => <Component {...props} />}
+    />
+  );
+};
+
+const CustomRouter = () => {
+  return (
+    <Routes>
+      <CustomRoute path={'/'} component={Home} />
+    </Routes>
+  );
+};
 
 function App() {
   return <div className='App'>Boiler Plate</div>;
