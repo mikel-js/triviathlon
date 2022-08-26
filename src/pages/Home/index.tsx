a58c40bdf3cff6606e214942602518ea8a289e7a
mikel-js <michaelcastro.js@gmail.com>
2022-08-26 20:50:53 +0300

    feat: add title in home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const index = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Triviathlon</h1>
+    </div>
+  );
+};
 
-export default index
+export default index;
