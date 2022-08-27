1cf027b1450e2334ee26cb2aebd2f8609f0f91fa
mikel-js <michaelcastro.js@gmail.com>
2022-08-27 17:25:09 +0300

    feat: add constants and some style

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const color = {
+  ORANGE1: '#f58a41',
+  YELLOW1: '#f1eb9f',
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import { color } from '../../constants';
+
+const Section = styled.div`
+  min-height: 100%;
+  height: 100vh;
+`;
+
+const StyledHeading = styled.div`
+  background-color: ${color.ORANGE1};
+  height: 30vh;
+`;
+
+const StyledContent = styled.div`
+  background-color: ${color.YELLOW1};
+  height: 70vh;
+`;
 
 const index = () => {
   return (
-    <div>
-      <h1>Triviathlon</h1>
-      <p>Test your mental endurance</p>
-    </div>
+    <Section>
+      <StyledHeading>
+        <h1>Triviathlon</h1>
+        <p>Test your mental endurance</p>
+      </StyledHeading>
+      <StyledContent>content</StyledContent>
+    </Section>
   );
 };
 
