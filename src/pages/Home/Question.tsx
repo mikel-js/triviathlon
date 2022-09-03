a24cfd3ac59826f698ccfabc2c4d6853c74f9a19
mikel-js <michaelcastro.js@gmail.com>
2022-09-03 10:34:51 +0300

    feat: add questionCard

--- src/pages/Home/Question.tsx
+++ src/pages/Home/Question.tsx
@@ -3,6 +3,24 @@ import styled from 'styled-components';
 import { Difficulty } from '../../API';
 import { Level, questionObject } from './Discover';
 
+const StyledQuestion = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledQuestionContainer = styled.div<{ bgColor: string }>`
+  display: flex;
+  flex-direction: column;
+  max-width: 75%;
+  padding: 2em;
+  border: solid;
+  border-radius: 20px;
+  margin: 1em 0;
+
+  ${({ bgColor }) => `background-color: ${bgColor}`}
+`;
+
 const Question: React.FC<{
   difficulty: Difficulty;
   questions: Level;
@@ -11,15 +29,24 @@ const Question: React.FC<{
     if (questions) return questions[difficulty];
   };
 
+  const cardColorMap = {
+    easy: 'green',
+    medium: 'yellow',
+    hard: 'red',
+  };
+
   const question = getQuestion(difficulty);
 
   return (
-    <div>
-      <h1 dangerouslySetInnerHTML={{ __html: question?.question || '' }}></h1>
-      {question?.choices.map((choice) => (
-        <p>{choice}</p>
-      ))}
-    </div>
+    <StyledQuestion>
+      <StyledQuestionContainer bgColor={cardColorMap[difficulty]}>
+        <h1>{difficulty.toUpperCase()}</h1>
+        <h2 dangerouslySetInnerHTML={{ __html: question?.question || '' }}></h2>
+        {question?.choices.map((choice) => (
+          <p>{choice}</p>
+        ))}
+      </StyledQuestionContainer>
+    </StyledQuestion>
   );
 };
 
