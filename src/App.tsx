0865df4936489d65021c72302a799538f4f77f6c
mikel-js <michaelcastro.js@gmail.com>
2022-09-06 23:34:53 +0300

    feat: removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, FC } from 'react';
+import React, { lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('./pages/Home/index'));
