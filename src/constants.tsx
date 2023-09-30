05429f3aa49edc8590a0c50cb3b75256fc802d6d
mikel-js <michaelcastro.js@gmail.com>
2023-09-30 22:02:52 +0300

    remove duplicate file

--- src/constants.tsx
+++ src/constants.tsx
@@ -5,3 +5,14 @@ export const breakpoint = {
   lg: '992px',
   xl: '1200px',
 };
+
+export const color = {
+  ORANGE1: '#f58a41',
+  YELLOW1: '#f1eb9f',
+  PURPLE1: '#cc99ff',
+  BROWN1: '#dfbe9f',
+  BLUE1: '#b3ccff',
+  PINK1: '#ffcccc',
+  RED1: '#ff4d4d',
+  RED2: '#ffb3b3',
+};
