c217d7858673f18697d6afb96746aa19d4e6f36f
mikel-js <michaelcastro.js@gmail.com>
2023-10-01 21:05:49 +0300

    Add theme context

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export type Theme = 'Default' | 'Dark Mode' | 'Fresh';
+
+export type ThemeContextType = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>({
+  theme: 'Default',
+  setTheme: () => {},
+});
+
+export const ThemeProvider: React.FC<React.PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const [theme, setTheme] = useState<Theme>('Default');
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  return context;
+};
